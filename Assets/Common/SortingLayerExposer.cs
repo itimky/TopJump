71437bd058cc2019a68fa07b7b354090c146b3fe
Timky <iTimky@gmail.com>
2015-11-21 15:22:54 +0300

    CoinX2, ScoreX2, MagnetField BoxCollider2D size fix

---
+++ Assets/Common/SortingLayerExposer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SortingLayerExposer : MonoBehaviour
+{
+
+    public string SortingLayerName = "Default";
+    public int SortingOrder = 1;
+
+    void Awake()
+    {
+        var meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        meshRenderer.sortingLayerName = SortingLayerName;
+        meshRenderer.sortingOrder = SortingOrder;
+    }
+}
