b1823731dd8745e8488971188cdb65000a312dda
Timky <iTimky@gmail.com>
2015-11-24 23:59:39 +0300

    Aspect ratio & UI fixes

---
+++ Assets/Scenes/CameraSizeController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSizeController : MonoBehaviour
+{
+    void Awake()
+    {
+        var mainCamera = Camera.main;
+        var aspectRatio = 1 / mainCamera.aspect;
+        if (aspectRatio < 1.7)
+            return;
+
+        mainCamera.transform.localPosition += new Vector3(0, 2, 0);
+        mainCamera.orthographicSize += 2;
+    }
+
+}
---
+++ Assets/Scenes/FrameRateController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrameRateController : MonoBehaviour
+{
+
+    public int TargetFps;
+
+    void Awake()
+    {
+        Application.targetFrameRate = TargetFps;
+    }
+}
