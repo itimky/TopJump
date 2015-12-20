38b90c41f2aba749d86b8af2b703b93619cab044
Timky <iTimky@gmail.com>
2015-12-20 14:23:51 +0300

    perspective camera

--- Assets/Backgroud/Clouds/CloudManager.cs
+++ Assets/Backgroud/Clouds/CloudManager.cs
@@ -6,142 +6,145 @@ using System.Linq;
 public class CloudManager : MonoBehaviour
 {
 
-    public Transform CloudPrefab_1;
-    public Transform CloudPrefab_2;
-    public Transform CloudPrefab_3;
-    public Transform CloudPrefab_4;
-    List<Transform> prefabs;
-    List<Cloud> AllClouds;
+	public Transform CloudPrefab_1;
+	public Transform CloudPrefab_2;
+	public Transform CloudPrefab_3;
+	public Transform CloudPrefab_4;
+	List<Transform> prefabs;
+	List<Cloud> AllClouds;
 
-    int direction;
-    public int paddingX;
-    public int verticalDistance;
-    public int deltaY;
-    public int deltaX;
+	int direction;
+	public int paddingX;
+	public int verticalDistance;
+	public int deltaY;
+	public int deltaX;
 
-    public int OffsetY;
-    private int nextY;
+	public int OffsetY;
+	private int nextY;
 
-    public int InstanceCount;
+	public int InstanceCount;
+	public int FarDistance;
 
-    void Start()
-    {
+	void Start()
+	{
 //        nextY = verticalDistance;
-        prefabs = new List<Transform>();
-        prefabs.Add(CloudPrefab_1);
-        prefabs.Add(CloudPrefab_2);
-        prefabs.Add(CloudPrefab_3);
-        prefabs.Add(CloudPrefab_4);
-        if (Random.Range(0, 2) == 1)
-            direction = 1;
-        else
-            direction = -1;
+		prefabs = new List<Transform>();
+		prefabs.Add(CloudPrefab_1);
+		prefabs.Add(CloudPrefab_2);
+		prefabs.Add(CloudPrefab_3);
+		prefabs.Add(CloudPrefab_4);
+		if (Random.Range(0, 2) == 1)
+			direction = 1;
+		else
+			direction = -1;
 
-        AllClouds = new List<Cloud>();
+		AllClouds = new List<Cloud>();
 
-        CreateClouds();
-        InvokeRepeating("CheckClouds", 1f, 0.2f);
-    }
+		CreateClouds();
+		InvokeRepeating("CheckClouds", 1f, 0.2f);
+	}
 
-    void CheckClouds()
-    {
+	void CheckClouds()
+	{
 
-        List<Cloud> cloudsHorizontalUpdate;
-        if (direction > 0)
-        {
-            var maxX = Game.positions[Game.positions.Count - 1];
-            cloudsHorizontalUpdate = AllClouds.Where(c => c.Transform.position.x > maxX + paddingX).ToList();
-            foreach (var cloud in cloudsHorizontalUpdate)
-                SetPosition(cloud);            
-        }
-        else
-        {
-            var minX = Game.positions[0];
-            cloudsHorizontalUpdate = AllClouds.Where(c => c.Transform.position.x < minX - paddingX).ToList();
-            foreach (var cloud in cloudsHorizontalUpdate)
-                SetPosition(cloud);
-        }
+		List<Cloud> cloudsHorizontalUpdate;
+		if (direction > 0)
+		{
+			var maxX = Game.positions[Game.positions.Count - 1];
+			cloudsHorizontalUpdate = AllClouds.Where(c => c.Transform.position.x > maxX + paddingX).ToList();
+			foreach (var cloud in cloudsHorizontalUpdate)
+				SetPosition(cloud);            
+		}
+		else
+		{
+			var minX = Game.positions[0];
+			cloudsHorizontalUpdate = AllClouds.Where(c => c.Transform.position.x < minX - paddingX).ToList();
+			foreach (var cloud in cloudsHorizontalUpdate)
+				SetPosition(cloud);
+		}
 
 
 
-        var cloudsHeightUpdate = AllClouds.Where(c => PlayerInfo.maxTraveled - c.Height > OffsetY).ToList();
-        if (cloudsHeightUpdate.Count == 0)
-            return;
+		var cloudsHeightUpdate = AllClouds.Where(c => PlayerInfo.maxTraveled - c.Height > OffsetY).ToList();
+		if (cloudsHeightUpdate.Count == 0)
+			return;
 
-        foreach (var cloud in cloudsHeightUpdate)
-        {
-            cloud.Height = nextY;
-            SetPosition(cloud);
-        }
+		foreach (var cloud in cloudsHeightUpdate)
+		{
+			cloud.Height = nextY;
+			SetPosition(cloud);
+		}
 
-        nextY += verticalDistance;
-    }
+		nextY += verticalDistance;
+	}
 
-    void CreateClouds()
-    {
-        AllClouds = new List<Cloud>();
+	void CreateClouds()
+	{
+		AllClouds = new List<Cloud>();
 
-        int i = 0;
-        while (AllClouds.Count < InstanceCount)
-        {
-            var prefab = prefabs[i];
-            var gameObject = Game.MakePrefabInstance(prefab);
-            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(3 * direction, 0);
+		int i = 0;
+		while (AllClouds.Count < InstanceCount)
+		{
+			var prefab = prefabs[i];
+			var gameObject = Game.MakePrefabInstance(prefab);
+			var goTransform = gameObject.transform;
+			goTransform.localPosition = new Vector3(goTransform.localPosition.x, goTransform.localPosition.y, FarDistance);
+			gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(3 * direction, 0);
 
-            var cloud = new Cloud(gameObject.transform, nextY);
-            SetPosition(cloud);
-            AllClouds.Add(cloud);
-            i++;
+			var cloud = new Cloud(gameObject.transform, nextY);
+			SetPosition(cloud);
+			AllClouds.Add(cloud);
+			i++;
 
-            if (i % 2 != 0)
-                nextY += verticalDistance;
+			if (i % 2 != 0)
+				nextY += verticalDistance;
 
-            if (i == prefabs.Count)
-                i = 0;
-        }
+			if (i == prefabs.Count)
+				i = 0;
+		}
 
-        var sign = direction > 0 ? -1 : 1;
-        foreach (var cloud in AllClouds)
-        {
-            var scale = Random.Range(1, 4);
-            cloud.Transform.localScale = new Vector3(scale, scale, 1);
-            cloud.Transform.position += new Vector3(sign * Random.Range(0, deltaX + 1), 0, 0);
-        }
-        AllClouds.Shuffle();
-    }
+		var sign = direction > 0 ? -1 : 1;
+		foreach (var cloud in AllClouds)
+		{
+			var scale = Random.Range(1, 4);
+			cloud.Transform.localScale = new Vector3(scale, scale, 1);
+			cloud.Transform.position += new Vector3(sign * Random.Range(0, deltaX + 1), 0, 0);
+		}
+		AllClouds.Shuffle();
+	}
 
-    void SetPosition(Cloud cloud)
-    {
-        var padding = 30;
-        int yDelta = Random.Range(0, deltaY + 1);
-        if (direction > 0)
+	void SetPosition(Cloud cloud)
+	{
+		var padding = 30;
+		int yDelta = Random.Range(0, deltaY + 1);
+		if (direction > 0)
             //65318.88
             //20105.95
-        {
-            var minX = Game.positions[0];
-            cloud.Transform.position = new Vector3(minX - paddingX, cloud.Height + yDelta, cloud.Transform.position.z);
-            if (AllClouds.Any(c => !ReferenceEquals(c, cloud) && c.Height == cloud.Height && c.Transform.position.x == cloud.Transform.position.x))// && Mathf.Abs(c.Transform.position.x - cloud.Transform.position.x) < float.Epsilon))
+		{
+			var minX = Game.positions[0];
+			cloud.Transform.position = new Vector3(minX - paddingX, cloud.Height + yDelta, cloud.Transform.position.z);
+			if (AllClouds.Any(c => !ReferenceEquals(c, cloud) && c.Height == cloud.Height && c.Transform.position.x == cloud.Transform.position.x))// && Mathf.Abs(c.Transform.position.x - cloud.Transform.position.x) < float.Epsilon))
                 cloud.Transform.position -= new Vector3(padding, 0, 0);
-        }
-        else
-        {
-            var maxX = Game.positions[Game.positions.Count - 1];
-            cloud.Transform.position = new Vector3(maxX + paddingX, cloud.Height + yDelta, cloud.Transform.position.z);
-            if (AllClouds.Any(c => !ReferenceEquals(c, cloud) && c.Height == cloud.Height && c.Transform.position.x == cloud.Transform.position.x))//Mathf.Abs(c.Transform.position.x - cloud.Transform.position.x) < float.Epsilon))
+		}
+		else
+		{
+			var maxX = Game.positions[Game.positions.Count - 1];
+			cloud.Transform.position = new Vector3(maxX + paddingX, cloud.Height + yDelta, cloud.Transform.position.z);
+			if (AllClouds.Any(c => !ReferenceEquals(c, cloud) && c.Height == cloud.Height && c.Transform.position.x == cloud.Transform.position.x))//Mathf.Abs(c.Transform.position.x - cloud.Transform.position.x) < float.Epsilon))
                 cloud.Transform.position += new Vector3(padding, 0, 0);
-        }
-    }
+		}
+	}
 }
 
 
 public class Cloud
 {
-    public readonly Transform Transform;
-    public int Height;
+	public readonly Transform Transform;
+	public int Height;
 
-    public Cloud(Transform cloud, int height)
-    {
-        Transform = cloud;
-        Height = height;
-    }
+	public Cloud(Transform cloud, int height)
+	{
+		Transform = cloud;
+		Height = height;
+	}
 }
--- Assets/Scenes/TopJump/Game.cs
+++ Assets/Scenes/TopJump/Game.cs
@@ -1,99 +1,103 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngineInternal;
 
 public class Game : MonoBehaviour
 {
-    public static bool IsPaused { get; private set; }
+	public static bool IsPaused { get; private set; }
 
-    public static bool IsGameOver { get; private set; }
+	public static bool IsGameOver { get; private set; }
 
-    private static List<Pausable> PausableObjects;
-    private static List<IGameOverHandler> GameOverHandlers;
-    public static readonly List<float> positions = new List<float> { -4f, 0f, 4f };
+	private static List<Pausable> PausableObjects;
+	private static List<IGameOverHandler> GameOverHandlers;
+	public List<float> Pos;
+	//	public static readonly List<float> positions = new List<float> { -4f, 0f, 4f };
+	public static List<float> positions { get; private set; }
 
-    void Awake()
-    {
-        Screen.fullScreen = false;
-        PausableObjects = new List<Pausable>();
-        GameOverHandlers = new List<IGameOverHandler>();
-    }
+	void Awake()
+	{
+		positions = Pos;
+		Screen.fullScreen = false;
+		PausableObjects = new List<Pausable>();
+		GameOverHandlers = new List<IGameOverHandler>();
+	}
 
-    void Start()
-    {
-        Resume();
-    }
-
-
-    public static GameObject MakePrefabInstance(GameObject prefab)
-    {
-        var instance = Instantiate(prefab);
-        instance.transform.localPosition = new Vector2(0, -60);
-        return instance;
-    }
-
-    public static Transform MakePrefabInstance(Transform prefab)
-    {
-        var instance = Instantiate(prefab);
-        instance.localPosition = new Vector2(0, -60);
-        return instance;
-    }
-
-    public static RectTransform MakePrefabInstance(RectTransform prefab)
-    {
-        return Instantiate(prefab);
-    }
+	void Start()
+	{
+		Resume();
+	}
 
 
-    public static void RegisterPausableObject(Pausable pausable)
-    {
-        if (!PausableObjects.Contains(pausable))
-            PausableObjects.Add(pausable);
-    }
+	public static GameObject MakePrefabInstance(GameObject prefab)
+	{
+		var instance = Instantiate(prefab);
+		instance.transform.localPosition = new Vector2(0, -60);
+		return instance;
+	}
 
-    public static void RegisterGameOverHandler(IGameOverHandler handler)
-    {
-        if (!GameOverHandlers.Contains(handler))
-            GameOverHandlers.Add(handler);
-    }
+	public static Transform MakePrefabInstance(Transform prefab)
+	{
+		var instance = Instantiate(prefab);
+		instance.localPosition = new Vector2(0, -60);
+		return instance;
+	}
 
-
-    public static void Pause()
-    {
-        if (IsPaused)
-            return;
-
-        IsPaused = true;
-        MenuPanelController.ShowAsPause();
-        PausableObjects.ForEach(po => po.Pause());
-    }
-
-    public void PauseNonStatic()
-    {
-        Pause();
-    }
-
-    public static void Resume()
-    {
-        if (!IsPaused)
-            return;
-
-        IsPaused = false;
-        IsGameOver = false;
-        PausableObjects.ForEach(po => po.Resume());
-    }
+	public static RectTransform MakePrefabInstance(RectTransform prefab)
+	{
+		return Instantiate(prefab);
+	}
 
 
-    public static void GameOver()
-    {
-        IsPaused = true;
-        IsGameOver = true;
-        MenuPanelController.ShowAsGameOver();
-        PausableObjects.ForEach(po => po.Pause());
+	public static void RegisterPausableObject(Pausable pausable)
+	{
+		if (!PausableObjects.Contains(pausable))
+			PausableObjects.Add(pausable);
+	}
 
-        GameOverHandlers.ForEach(goh => goh.OnGameOver());
-        PlayerPrefs.Save();
-    }
+	public static void RegisterGameOverHandler(IGameOverHandler handler)
+	{
+		if (!GameOverHandlers.Contains(handler))
+			GameOverHandlers.Add(handler);
+	}
+
+
+	public static void Pause()
+	{
+		if (IsPaused)
+			return;
+
+		IsPaused = true;
+		MenuPanelController.ShowAsPause();
+		PausableObjects.ForEach(po => po.Pause());
+	}
+
+	public void PauseNonStatic()
+	{
+		Pause();
+	}
+
+	public static void Resume()
+	{
+		if (!IsPaused)
+			return;
+
+		IsPaused = false;
+		IsGameOver = false;
+		PausableObjects.ForEach(po => po.Resume());
+	}
+
+
+	public static void GameOver()
+	{
+		IsPaused = true;
+		IsGameOver = true;
+		MenuPanelController.ShowAsGameOver();
+		PausableObjects.ForEach(po => po.Pause());
+
+		GameOverHandlers.ForEach(goh => goh.OnGameOver());
+		PlayerPrefs.Save();
+	}
 
 
 
@@ -102,18 +106,18 @@ public class Game : MonoBehaviour
 
 public abstract class Pausable : MonoBehaviour
 {
-    public virtual void Pause()
-    {
-        enabled = false;
-    }
+	public virtual void Pause()
+	{
+		enabled = false;
+	}
 
-    public virtual void Resume()
-    {
-        enabled = true;
-    }
+	public virtual void Resume()
+	{
+		enabled = true;
+	}
 }
 
 public interface IGameOverHandler
 {
-    void OnGameOver();
+	void OnGameOver();
 }
